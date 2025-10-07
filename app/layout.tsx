606c49dd9872c2c483a220bfb485e95ede5abdfd
battleonbase-pro <battleonbase@gmail.com>
2025-10-08 00:35:31 +0400

    fix(meta): update deprecated apple-mobile-web-app-capable to mobile-web-app-capable

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,7 +67,7 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/favicon.ico" />
         <link rel="apple-touch-icon" href="/Base_basemark_blue.png" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
         <meta name="apple-mobile-web-app-title" content="NewsCast Debate" />
       </head>
