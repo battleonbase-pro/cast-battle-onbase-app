386b869975de6e711ee89935730f6746b14f81f9
battleonbase-pro <battleonbase@gmail.com>
2025-10-20 01:42:07 +0400

    Disable user zooming while keeping scrolling enabled

--- app/layout.tsx
+++ app/layout.tsx
@@ -62,8 +62,8 @@ export const metadata: Metadata = {
 export const viewport = {
   width: "device-width",
   initialScale: 1,
-  maximumScale: 5,
-  userScalable: true,
+  maximumScale: 1,
+  userScalable: false,
   themeColor: "#0052FF", // Base blue color
 };
 
